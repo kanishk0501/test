43a1f478bfe30144992724a6965bfce89b241dfb
kanishk <kanishk0501@gmail.com>
2021-02-07 18:04:04 +0530

    second one :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Canvas } from 'react-three-fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const Box = () => {
+        return (
+            <mesh>
+                <boxBufferGeometry attach='geometry' />
+                <meshLambertMaterial attach='material' color='hotpink' />
+            </mesh>
+        );
+    };
+    return (
+        <Canvas>
+            <OrbitControls />
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 15, 10]} angle={0.3} />
+            <Box />
+            <Stars />
+        </Canvas>
+    );
 }
 
 export default App;
